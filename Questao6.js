a2851282346a66f979987174bb751b1dd5bd65f5
Rafas-ms <rafael.martinssilva@hotmail.com>
2021-03-17 15:44:16 -0300

    Exercício 6

---
+++ Questao6.js
@@ -0,0 +1,8 @@
+let nomes = prompt("Digite varios nomes: ");
+function contrario(nomes_s){
+    nomes = nomes_s.split(",");
+    nomes.reverse();
+   
+    return nomes;
+}
+console.log(contrario(nomes));
