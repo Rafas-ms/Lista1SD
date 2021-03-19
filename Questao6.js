a909bcebd18d3a81efe0ee323a546199e7625b4e
Rafas-ms <rafael.martinssilva@hotmail.com>
2021-03-19 13:30:21 -0300

    Exercício 7

--- Questao6.js
+++ Questao6.js
@@ -2,7 +2,6 @@ let nomes = prompt("Digite varios nomes: ");
 function contrario(nomes_s){
     nomes = nomes_s.split(",");
     nomes.reverse();
-   
     return nomes;
 }
 console.log(contrario(nomes));
---
+++ Questao7.js
@@ -0,0 +1,5 @@
+let pessoa = {
+        nome: "Rafael",
+        idade: 18,
+        peso:72
+}
