5fe905fab49e2ebdbbb6961246ec4ce3f4ea9e6d
Rafas-ms <rafael.martinssilva@hotmail.com>
2021-03-19 14:09:45 -0300

    Exercício 14

---
+++ Questao14.js
@@ -0,0 +1,3 @@
+let valor = prompt("Digite o valor do produto: ");
+if(valor>=100 && valor<=200) console.log("O valor do produto esta entre 100 e 200 reais!");
+else console.log("O valor do produto nao esta entre 100 e 200 reais!");
